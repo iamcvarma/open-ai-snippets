5975462b6fab3bf40211da880111c8199e1fa4f4
Chaitanya <iamcvers@gmail.com>
2022-11-14 17:27:21 +0530

    added fastapi with validation

--- app/copykitt.py
+++ app/copykitt.py
@@ -4,14 +4,21 @@ import config
 import argparse
 import re
 
+MAX_INPUT_LENGTH = 32
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input','-i',type=str, required=True)
     args = parser.parse_args()
     user_input = args.input
-    if len(user_input)>30:
-        return
-    print(generate_branding_keywords(user_input))
+    if validate_length(user_input):
+        generate_branding_snippet(user_input)
+        generate_branding_keywords(user_input)
+    else:
+        raise ValueError(f'Input length is too long.Must be under {MAX_INPUT_LENGTH}. Submitted input is {user_input}')
+
+def validate_length(prompt:str)->bool:
+    return len(prompt)<=MAX_INPUT_LENGTH
 
 def generate_branding_snippet(subject:str):
     openai.api_key = config.OPENAI_API_KEY
@@ -21,6 +28,7 @@ def generate_branding_snippet(subject:str):
     branding_text = response['choices'][0]['text'].strip()
     if branding_text[-1].isalpha():
         branding_text+='...'
+    print(f'Snippet: {branding_text}')
     return branding_text
 
 def generate_branding_keywords(subject:str):
@@ -31,7 +39,9 @@ def generate_branding_keywords(subject:str):
     keyword_text = response['choices'][0]['text'].strip()
     
     keyword_array = re.split(',|\n|;|-',keyword_text)
-    return list(map(lambda x:x.lower().strip(),keyword_array))
+    keyword_array_result= list(filter(lambda x:len(x),map(lambda x:x.lower().strip(),keyword_array)))
+    print(f'Keywords:{keyword_array_result}')
+    return keyword_array_result
 
 if __name__=="__main__":
     main()
---
+++ app/copykitt_api.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI,HTTPException
+from copykitt import generate_branding_keywords,generate_branding_snippet
+app = FastAPI()
+
+MAX_INPUT_LENGTH = 32
+
+@app.get('/generate_snippet')
+async def generate_snippet_api(prompt:str):
+    validate_input(prompt)
+    snippet = generate_branding_snippet(prompt)
+    return {"snippet":snippet}
+
+
+@app.get('/generate_keywords')
+async def generate_keywords_api(prompt:str):
+    validate_input(prompt)
+    keywords = generate_branding_keywords(prompt)
+    return {"keywords":keywords}
+
+
+@app.get('/generate_snippet_and_keywords')
+async def generate_snippet_and_keywords_api(prompt:str):
+    validate_input(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_branding_keywords(prompt)
+    return {"snippet":snippet,"keywords":keywords}
+
+
+def validate_input(prompt):
+    if len(prompt)>=MAX_INPUT_LENGTH:
+        raise HTTPException(
+            status_code=400,
+            detail=f'Input string too long. Must be under {MAX_INPUT_LENGTH} characters.'
+        )
